87c3022d54f072a19ff177a49f51be12165f0d48
Howon Lee <howon@howonlee.com>
2016-07-19 20:09:20 -0700

    add the rack

--- Rack.lua
+++ Rack.lua
@@ -1,6 +1,6 @@
-local Walk, parent = torch.class('nn.Walk', 'nn.Module')
+local Rack, parent = torch.class('nn.Rack', 'nn.Module')
 
-function Walk:__init(alpha, inputSize)
+function Rack:__init(alpha, inputSize)
    parent.__init(self)
    self.alpha = alpha or 1
    self.inputSize = inputSize
@@ -9,13 +9,13 @@ function Walk:__init(alpha, inputSize)
    self.noise:uniform(-alpha, alpha)
 end
 
-function Walk:updateOutput(input)
+function Rack:updateOutput(input)
    self.output:resizeAs(input):copy(input)
    self.output = self.output + self.noise:view(1, self.inputSize):expandAs(self.output)
    return self.output
 end
 
-function Walk:updateGradInput(input, gradOutput)
+function Rack:updateGradInput(input, gradOutput)
    self.gradInput:resizeAs(gradOutput):copy(gradOutput)
    return self.gradInput
 end
